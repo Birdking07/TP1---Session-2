82c0f16fe512ebb3bbe6483b89f20707da61356f
Bird <ninjabirdpepito@gmail.com>
2022-02-02 15:12:21 -0500

    Fixed Prompt.java

--- 2CP-TP1-RushHour/src/ca/qc/bdeb/info/Prompt.java
+++ 2CP-TP1-RushHour/src/ca/qc/bdeb/info/Prompt.java
@@ -33,8 +33,8 @@ public class Prompt {
      */
     public Command readUserCommand(Challenge challenge) {
 
+       Command PromptCommand = new Command();
 
-
-        return ;
+        return PromptCommand;
     }
 }
